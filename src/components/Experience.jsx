4654e381fee787b2718ce974c626190724f087a9
alexander <alex43bg@gmail.com>
2024-01-13 17:23:47 +0200

    13.01 17.23-colors-setup-finish

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,14 @@
 import { OrbitControls } from '@react-three/drei'
 // import { Ball } from './Ball/Ball_my_site'
 // import { Ball1 } from './Ball/Ball_my_site'
-import { Ball_tx } from './ball/Ball_tx'
-
+// import { Ball_tx } from './ball/Ball_tx'
+import { Ball_tx_lite } from './ball/Ball_tx_lite'
 export const Experience = () => {
 	return (
 		<>
 			<OrbitControls enableZoom={true} />
 			{/* <Header/> */}
-			<Ball_tx />
+			<Ball_tx_lite />
 		</>
 	)
 }
