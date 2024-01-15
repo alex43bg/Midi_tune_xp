c56acef330cf37ee42a5436efdbc772cf3cc8f44
alexander <alex43bg@gmail.com>
2024-01-15 15:55:56 +0200

    15.01_camera_first

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,6 @@
 import { OrbitControls } from '@react-three/drei'
-// import { Ball } from './Ball/Ball_my_site'
-// import { Ball1 } from './Ball/Ball_my_site'
-// import { Ball_tx } from './ball/Ball_tx'
 import { Ball_tx_lite } from './ball/Ball_tx_lite'
+
 export const Experience = () => {
 	return (
 		<>
