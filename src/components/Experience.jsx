8828377a17876426ce2f7a5fb8a9dfd0a2a91d74
alexander <alex43bg@gmail.com>
2024-01-17 16:21:00 +0200

    lightning_setup_start

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,10 @@
 import {Ball} from './ball/Ball_my_site'
-import {Ball_tx_lite} from "./ball/Ball_tx_lite.jsx";
+// import {Ball_tx_lite} from "./ball/Ball_tx_lite.jsx";
 
 export const Experience = () => {
     return (
         <>
-            <Ball_tx_lite/>
+            <Ball/>
         </>
     )
 }
